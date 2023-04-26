ea04270dc42188bb69feda6bb5e6b6d9beccd02c
keske <keske@LAPTOP-S77GC77O>
2023-04-25 21:29:11 -0500

    Add project files.

---
+++ Login-Register-App/Controllers/AccountController1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Login_Register_App.Controllers;
+using Login_Register_App.Models;
+
+namespace Login_Register_App.Controllers
+{
+    public class AccountController1 : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Verify(Account acc)
+        {
+            return View();
+        }
+    }
+}
---
+++ Login-Register-App/Models/Account.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Login_Register_App.Models
+{
+    public class Account
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? LastName { get; set; }
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+    }
+}
